2dd27021b3f534839d7585fd2767f16662836109
Vladyslav.Levishchen <2751lev@gmail.com>
2024-11-22 15:43:39 +0200

    DZ 5.3

---
+++ DZ5.3.py
@@ -0,0 +1,18 @@
+# 'Python Community' -> #PythonCommunity
+# 'i like python community!' -> #ILikePythonCommunity
+# 'Should, I. subscribe? Yes!' -> #ShouldISubscribeYes
+import string
+a = input()
+a1 = a.title()
+b = "#"
+for i in a1:
+    if i in string.punctuation or i == " ":
+        continue
+    b += i
+print(b[:139])
+
+
+
+
+str("hello")
+
