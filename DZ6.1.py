21af14aafd67d31168c8961d700d0c64d1e7ee9a
Vladyslav.Levishchen <2751lev@gmail.com>
2024-11-26 14:58:35 +0200

    DZ 6.1

--- DZ6.1.py
+++ DZ6.1.py
@@ -4,11 +4,6 @@
 # "a-A" -> abcdefghijklmnopqrstuvwxyzA
 
 import string
-
-print(string.ascii_letters)
-a = input()
-b = int(string.ascii_letters.find(a[0]))
-c = int(string.ascii_letters.find(a[-1]))
-while b <= c:
-    print(string.ascii_letters[b], end='')
-    b += 1
+a = input("введіть діапазон букв через дефіс: ")
+s = str(string.ascii_letters[int(string.ascii_letters.find(a[0])):int(string.ascii_letters.find(a[-1]))+1])
+print(s)
